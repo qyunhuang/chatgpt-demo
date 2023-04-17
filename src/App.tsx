46d444f2d83990ac8e65c0e021ba1a64ff730bba
spacey <3354909082@qq.com>
2023-04-17 22:31:52 +0800

    chatgpt

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import { config } from './config';
+import { ChatGPTAPI } from 'chatgpt';
 
-function App() {
+const App = () => {
+  const [text, setText] = React.useState<undefined | string>("");
+
+  React.useEffect(() => {
+    const test = async () => {
+      const api = new ChatGPTAPI({
+        apiKey: config.api,
+      });
+
+      const res = await api.sendMessage('write a joke!');
+      setText(res.text);
+      console.log(res.text)
+    };
+
+    test();
+  }, []);
+
   return (
     <div className="App">
-      config: {JSON.stringify(config)}
+      {text}
     </div>
   );
 }
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
-export const config = {
-  api: process.env.REACT_APP_OPENAI_API_KEY,
+interface IConfig {
+  api: string
+}
+
+export const config: IConfig = {
+  api: process.env.REACT_APP_OPENAI_API_KEY as string,
 }
