dbca756f0d5c998843a7bad7179c899663112ad8
spacey <3354909082@qq.com>
2023-04-18 10:49:21 +0800

    proxy

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import { config } from './config';
 import { ChatGPTAPI } from 'chatgpt';
+import { HttpsProxyAgent } from "https-proxy-agent";
 
 const App = () => {
   const [text, setText] = React.useState<undefined | string>("");
 
   React.useEffect(() => {
     const test = async () => {
-      const api = new ChatGPTAPI({
+      const options = {
         apiKey: config.api,
+        debug: true,
+      };
+      const api = new ChatGPTAPI({
+        ...options, // Spread operator to include any additional options provided to ChatGPTAPI constructor
+        fetch: (url, options = {}) => {
+          const defaultOptions = {
+            agent: new HttpsProxyAgent({host: '127.0.0.1', port: 7890}), // Update to pass proxy server information correctly
+          }
+          const mergedOptions = {
+            ...defaultOptions,
+            ...options, // Spread operator to override defaultOptions with any options provided to the fetch function
+          }
+          return fetch(url, mergedOptions) // Return fetch function with merged options
+        },
       });
 
-      const res = await api.sendMessage('write a joke!');
+      const res = await api.sendMessage("tell me a joke");
+      console.log(res.text, 1);
       setText(res.text);
-      console.log(res.text)
     };
 
     test();
